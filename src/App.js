480ebd8b0eead91f1647035697f580b99485e08f
Matt Core <mcore@taelifesciences.com>
2020-09-25 16:21:30 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-	  <p>
-	  Inserting custom text here on 2020-09-25 at 12:23pm
-	  </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-	  <p> a second custom mod here at 1:01pm </p>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
