a841dc0d7076b280a29f31e552408fb8b5eeef14
Matt Core <mcore@taelifesciences.com>
2020-09-25 12:26:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+	  <p>
+	  Inserting custom text here on 2020-09-25 at 12:23pm
+	  </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
